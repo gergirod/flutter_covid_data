35fec091c5c3a1c5a333cc8b73b6fd05d0bc652b
gergirod <german.girod@creativecoefficient.net>
2020-04-29 22:01:11 +1000

    undo commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/germangirod/Downloads/flutter 3"
+export "FLUTTER_APPLICATION_PATH=/Users/germangirod/Documents/german/bloc_basics/flutter_bloc_covid"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/germangirod/Downloads/flutter 3/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
